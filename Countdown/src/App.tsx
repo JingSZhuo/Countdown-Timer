c1a0863b7f65e723635f69009bdaebc93557c61b
Jing Zhuo <jing2001@live.co.uk>
2023-07-12 17:44:49 +0100

    initial commit

---
+++ Countdown/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.scss';
+import './reset.scss';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default App
